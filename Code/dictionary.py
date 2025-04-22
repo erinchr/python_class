aeae57fb3a0ca7ca8f518b671ac5d92fd3a3c61f
erinchr <erinchr@xtra.co.nz>
2025-04-22 15:29:04 +1200

    Update dictionary.py
    Added Comments

--- Code/dictionary.py
+++ Code/dictionary.py
@@ -1,5 +1,5 @@
 id = {"name":"Alice", "age":25, "city":"Auckland",}
 mail = {"email":"alice@email.com"}
-id.update(mail)
-id["age"]+=1
-print(id)
+id.update(mail) #Adds mail to id dictionary
+id["age"]+=1 #Adds 1 to age
+print(id) #Prints dictionary
