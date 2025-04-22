0fa5874beec10c08b5d8615156c8ad1d3f4bc073
erinchr <erinchr@xtra.co.nz>
2025-04-22 15:21:16 +1200

    Update Number.py
    Added comments

--- Code/Number.py
+++ Code/Number.py
@@ -1,5 +1,5 @@
 number = [1,2,3,4,5,6,7,8,9,10]
 numbers = [11,12,13]
-number.remove(5)
+number.remove(5) #Removes the 5
 for i in number + numbers: print(i)
-number.append(numbers)
+number.append(numbers) #Prints the numbers in the "number" and "numbers" list vertically 
